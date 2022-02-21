8c68a299ad935dce2a66140a7246c9383f832772
9oya <eido9oya@gmail.com>
2022-02-21 14:29:26 +0900

    Add greedy l1

--- ProblemSolving/main.swift
+++ ProblemSolving/main.swift
@@ -7,25 +7,8 @@
 
 import Foundation
 
-solution()
-
+print(solution())
 
 func solution() {
-    let now = "TUE 10:00"
     
-    let nComps = now.split(separator: " ")
-    let nWeek = nComps.first.map{"\($)"}.joined()
-    let trimmedString = string.trimmingCharacters(in: .whitespaces)
-    print(comvertWeekStrToInt(nWeek))
-}
-
-func comvertWeekStrToInt(_ w: String) -> Int {
-    let map: [String: Int] = [
-        "TUE": 3
-    ]
-    let key = w.uppercased()
-    if let num = map[key] {
-        return num
-    }
-    return 999
 }
