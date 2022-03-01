84d613a889ad056990fd9abd680f56cf999bc463
9oya <eido9oya@gmail.com>
2022-03-01 15:21:18 +0900

    Add a dynamic programming ps

---
+++ ProblemSolving/LeetCode/DynamicProgramming/ClimbingStairs_Easy.swift
@@ -0,0 +1,26 @@
+//
+//  ClimbingStairs.swift
+//  ProblemSolving
+//
+//  Created by Eido Goya on 2022/03/01.
+//
+
+import Foundation
+
+class ClimbingStairs {
+//    print(climbStairs(2)) // 2
+//    print(climbStairs(3)) // 3
+    
+    func climbStairs(_ n: Int) -> Int {
+        return stepUp(n, 1) + stepUp(n, 2)
+    }
+
+    func stepUp(_ target: Int, _ next: Int) -> Int {
+        if next > target {
+            return 0
+        } else if next == target {
+            return 1
+        }
+        return stepUp(target, next+1) + stepUp(target, next+2)
+    }
+}
