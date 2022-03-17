940d7b412873318e2fe5885b7ba4ad9c71409334
9oya <eido9oya@gmail.com>
2022-03-17 15:07:37 +0900

    Fix a dp ps

--- ProblemSolving/LeetCode/DynamicProgramming/ClimbingStairs_Easy.swift
+++ ProblemSolving/LeetCode/DynamicProgramming/ClimbingStairs_Easy.swift
@@ -11,16 +11,32 @@ class ClimbingStairs {
 //    print(climbStairs(2)) // 2
 //    print(climbStairs(3)) // 3
     
+    // Memoization
     func climbStairs(_ n: Int) -> Int {
-        return stepUp(n, 1) + stepUp(n, 2)
-    }
-
-    func stepUp(_ target: Int, _ next: Int) -> Int {
-        if next > target {
-            return 0
-        } else if next == target {
+        
+        var arr: [Int] = []
+        arr.append(1)
+        arr.append(1)
+        if n == 1 {
             return 1
         }
-        return stepUp(target, next+1) + stepUp(target, next+2)
+        for i in 2...n {
+            arr.append(arr[i-1] + arr[i-2])
+        }
+        return arr[n]
     }
+    
+    // Reculsion
+//    func climbStairs(_ n: Int) -> Int {
+//        return stepUp(n, 1) + stepUp(n, 2)
+//    }
+//
+//    func stepUp(_ target: Int, _ next: Int) -> Int {
+//        if next > target {
+//            return 0
+//        } else if next == target {
+//            return 1
+//        }
+//        return stepUp(target, next+1) + stepUp(target, next+2)
+//    }
 }
